130fdbe11637e60d837be51da4b9cee57ef6172b
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-03-26 05:24:49 +0200

    Fixed the last commit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,7 +15,7 @@ vim.keymap.set("n", "<A-q>", "<CMD>bdelete<CR>", { desc = "Close the current buf
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]], { desc = "Paste from system clipboard" })
 vim.keymap.set("n", "<leader>gs", "<cmd>Neogit<cr>", { desc = "Open neogit" });
-vim.keymap.set("n", "<leader>ds", "<cmd>Dotnet run<CR>", { desc = "Run the current dotnet project" })
+vim.keymap.set("n", "<leader>dr", "<cmd>Dotnet run<CR>", { desc = "Run the current dotnet project" })
 
 vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true })
 
