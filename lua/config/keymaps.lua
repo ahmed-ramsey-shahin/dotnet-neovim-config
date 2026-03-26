8bc229ae23f153aed38e421812ebef0ac7b651bd
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-03-26 05:23:24 +0200

    Remaped <leader>dr to run the current dotnet project using Dotnet run

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,12 +8,14 @@ vim.keymap.del({ "n", "t" }, "<C-/>")
 vim.keymap.del("n", "<leader>ft")
 vim.keymap.del("n", "<leader>fT")
 vim.keymap.del("n", "<leader>gs")
+vim.keymap.del("n", "<leader>dr");
 
 -- Custom keymaps
 vim.keymap.set("n", "<A-q>", "<CMD>bdelete<CR>", { desc = "Close the current buffer" })
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]], { desc = "Paste from system clipboard" })
 vim.keymap.set("n", "<leader>gs", "<cmd>Neogit<cr>", { desc = "Open neogit" });
+vim.keymap.set("n", "<leader>ds", "<cmd>Dotnet run<CR>", { desc = "Run the current dotnet project" })
 
 vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true })
 
