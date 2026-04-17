c8877c19c9e87f01daff77434004208f2e9f33e3
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-04-17 14:47:27 +0200

    Changed session manager to auto-session

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -1,23 +1,15 @@
 return {
     {
-        "folke/persistence.nvim",
-        event = "BufReadPre",
-        opts = { options = { "buffers", "curdir", "tabpages", "winsize" } },
-        keys = {
-            {
-                "<leader>qs",
-                function()
-                    require("persistence").load()
-                end,
-                desc = "Restore Session",
-            },
-            {
-                "<leader>ql",
-                function()
-                    require("persistence").load({ last = true })
-                end,
-                desc = "Restore Last Session",
-            },
+        "rmagatti/auto-session",
+        lazy = false,
+
+        ---enables autocomplete for opts
+        ---@module "auto-session"
+        ---@type AutoSession.Config
+        opts = {
+            allowed_dirs = vim.fn.glob("~/Documents/WS/*/", false, true),
+            git_use_branch_name = true,
+            git_auto_restore_on_branch_change = true,
         },
-    },
+    }
 }
